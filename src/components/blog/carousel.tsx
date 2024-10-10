6165fdcf8c469f06ddb876b6a5662a50782766f6
Writefreak <iscribbledit@gmail.com>
2024-10-10 19:36:23 +0100

    second commit

--- src/components/blog/carousel.tsx
+++ src/components/blog/carousel.tsx
@@ -9,8 +9,7 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
-import BlogHeader from "./carouselTitle";
-import CarouselTitle from "./carouselTitle";
+import { Button } from "../ui/button";
 
 export function CarouselDemo() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
@@ -25,12 +24,23 @@ export function CarouselDemo() {
               className="relative flex h-screen items-center justify-center"
             >
               <img
-                className="w-full  object-cover absolute"
+                className="w-full  object-cover absolute brightness-50"
                 alt=""
                 src={images[i].url}
               />
-              <div className=" relative z-10">
-                <CarouselTitle />
+              <div className="absolute flex flex-col items-center gap-5">
+                <h1
+                  className="text-6xl text-white font-bold text-center"
+                  dangerouslySetInnerHTML={{ __html: images[i].text }}
+                />
+
+                {/* the h1 above is written like that so my br tag below can work */}
+
+                <div>
+                  <Button className="translate-y-6 bg-white/10 backdrop-blur-sm shadow-sm border border-white hover:bg-white/20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300">
+                    View Blogs
+                  </Button>
+                </div>
               </div>
             </CarouselItem>
           ))}
@@ -45,22 +55,27 @@ export function CarouselDemo() {
 const images = [
   {
     id: 1,
-    url: `/images/man-jump-through-gaps-hills.jpg`,
+    url: `/images/open-book-beautiful-setting.jpg`,
+    text: `Immerse Yourself in a Vast World <br/> Where Stories Reside`,
   },
   {
     id: 2,
-    url: `/images/silhouette-standing-mountain-peak-sunrise-generated-by-ai.jpg`,
+    url: `/images/book-library-with-open-textbook.jpg`,
+    text: `Discover a Comprehensive <br/> Collection of Educative Materials`,
   },
   {
     id: 3,
-    url: `/images/young-blonde-woman-sitting-top-hill.jpg`,
+    url: `/images/lovely-friends-looking-laptop-indoors.jpg`,
+    text: `Easily Access Latest News <br/> And Trending Updates`,
   },
   {
     id: 4,
-    url: `/images/tourist-is-holding-through-binoculars-sunny-cloudy-sky-from-mountain-top.jpg`,
+    url: `/images/businessman-video-conference-call-while-working-from-home-coronavirus-pandemic.jpg`,
+    text: `Learn More About Effective <br/> And Life Saving Tips and Tricks`,
   },
   {
     id: 5,
-    url: `/images/professional-photographer-takes-photos-with-camera-tripod-rocky-peak-sunset-dark-tone.jpg`,
+    url: `/images/medium-shot-happy-people-meeting.jpg`,
+    text: `Express Your Thoughts <br/> As Part of a Large Community`,
   },
 ];
